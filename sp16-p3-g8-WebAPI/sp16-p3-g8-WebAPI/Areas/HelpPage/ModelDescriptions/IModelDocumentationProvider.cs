e775057a17aa5e26e8265597387a24e955c597b5
Pankaj Sherchan <pankaj.sherchan@outlook.com>
2016-03-30 13:24:12 -0500

    Create a new solution for web api

---
+++ sp16-p3-g8-WebAPI/sp16-p3-g8-WebAPI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace sp16_p3_g8_WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
