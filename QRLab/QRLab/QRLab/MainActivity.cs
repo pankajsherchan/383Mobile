d345b024b0bb001cc6d5e6bc13f8026022b89ce8
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2016-04-05 21:47:18 -0500

    My QR Work as it stands now...yes it's rather pathetic

---
+++ QRLab/QRLab/QRLab/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using ZXing.QrCode;
+using ZXing.Common;
+using ZXing;
+using Android.Media;
+using Android.Graphics;
+
+namespace QRLab {
+    [Activity(Label = "QRLab" , MainLauncher = true , Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+
+        TestQRController C;
+
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            ImageView IV = FindViewById<ImageView>(Resource.Id.QRExample);
+
+
+            //initialize
+            var options = new QrCodeEncodingOptions {
+                DisableECI = true ,
+                CharacterSet = "UTF-8" ,
+                Width = 100 ,
+                Height = 100
+            };
+
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+            writer.Encode("Hello world!");
+
+            Canvas c = new Canvas();
+            c.DrawBitmap();
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { Scan(IV); };
+        }
+        public void Scan(ImageView I) {
+            //Reader.decode()
+            Toast.MakeText(this , "QRCodeScanned!" , ToastLength.Short);
+        }
+    }
+}
+
