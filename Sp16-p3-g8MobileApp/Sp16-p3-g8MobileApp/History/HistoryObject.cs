cf642a98d2e97564a3847c14e68eaffbb9320ede
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2016-04-20 21:14:51 -0500

    Initial Movie History work

---
+++ Sp16-p3-g8MobileApp/Sp16-p3-g8MobileApp/History/HistoryObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+//Purpose is more or less to keep track of Local Data
+namespace Sp16p3g8MobileApp.History {
+    [JsonObject(MemberSerialization.OptIn)]
+    class HistoryObject {
+        //Identifying information
+        [JsonProperty]
+        public string MovieName { get; set; }
+        [JsonProperty]
+        public string ReleaseDate { get; set; }
+        [JsonProperty]
+        public int ReviewsPosted { get; set; }
+
+        //QR Code area
+        [JsonProperty]
+        List<string> ScannedCodes;
+
+        //Handly Little validator function!
+        public bool CanLeaveAReview() {
+            return ReviewsPosted > ScannedCodes.Count ? true : false;
+        }
+
+    }
+}
---
+++ Sp16-p3-g8MobileApp/Sp16-p3-g8MobileApp/HistoryPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Sp16p3g8MobileApp {
+    public class HistoryPage : ContentPage {
+        public HistoryPage() {
+            Content = new StackLayout {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
