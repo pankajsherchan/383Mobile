d8e935dd4747e06ca8151cdc72ead8cf0e7543d4
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2016-04-25 20:48:19 -0500

    Take two start on QR storage

---
+++ Sp16-p3-g8MobileApp/Sp16-p3-g8MobileApp/Data/TicketStorage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Sp16p3g8MobileApp.Data {
+    public static class TicketStorage {
+
+        public static List<string> GetTicketsFromStorage() {
+            //List<String> = DeserializeJSONObjects(Source); //DEJSONIFY!
+            throw new NotImplementedException();
+        }
+
+        public static void AddTicketToStorage(string s) {
+            s = JsonConvert.SerializeObject(s); //JSONIFY!
+            throw new NotImplementedException();
+        }
+    }
+}
