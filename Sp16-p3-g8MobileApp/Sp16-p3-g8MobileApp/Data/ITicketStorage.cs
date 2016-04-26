a38d7a5b74918b5a471e666ac5f4fb7eee1ca814
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2016-04-25 22:50:05 -0500

    In case nothing else gets done tonight, here's the more or less the structure and "1.0" version of loading/saving (at least if I'm reading the Docs correctly)
    No it will not work if you uncomment it, this is due to the fact that "GetFolderPath" for some reason doesn't work ( No it's not supposed to be in forms, according to the docs, and I tried anyway, same problem)
    
    iOS would not recognize the Forms Namespace unfortunately, not like I had any means of testing it....nor that we need it

---
+++ Sp16-p3-g8MobileApp/Sp16-p3-g8MobileApp/Data/ITicketStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sp16p3g8MobileApp {
+    public interface ITicketStorage {
+        void Save(string NewCode);
+        List<string> LoadCodes();
+    }
+}
