db80e2ea2e91e60224434bfb37dbded497b59823
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2016-04-26 23:49:31 -0500

    Android File I/O V 1.0 (iOS incoming, just gotta figure out how to get the file root)

---
+++ Sp16-p3-g8MobileApp/Droid/TicketStorageDroid.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Sp16p3g8MobileApp;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+[assembly: Dependency(typeof(ITicketStorage))]
+namespace Sp16p3g8MobileApp.Droid {
+    class TicketStorageDroid : ITicketStorage {
+        
+
+        public List<string> LoadCodes(string filename) {
+            
+            //I wish there was a "App data" area but docs 'll be fine for now
+            var rootPath = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDocuments);
+            string path = rootPath.AbsolutePath;
+            rootPath.Dispose();
+
+            var filePath = Path.Combine(path , filename);
+            string source = File.ReadAllText(path);
+
+            return JsonConvert.DeserializeObject<List<string>>(source); //DEJSONIFY!
+        }
+
+        //IMPORTANT! VALIDATE TO MAKE SURE THE SAME QR CODE HAS NOT BEEN SCANED BEFORE USING THIS METHOD!
+        public void Save(string filename, string NewCode) {
+            //List<string> codes = LoadCodes(filename);
+            //codes.Add(NewCode);
+            string JSONCode = JsonConvert.SerializeObject(NewCode); //JSONIFY!
+
+            var rootPath = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryDocuments);
+            string path = rootPath.AbsolutePath;
+            rootPath.Dispose();
+
+            var filePath = Path.Combine(path , filename);
+            File.AppendAllText(path , JSONCode);
+        }
+    }
+}
--- Sp16-p3-g8MobileApp/iOS/TicketStorageIOS.cs
+++ Sp16-p3-g8MobileApp/iOS/TicketStorageIOS.cs
@@ -2,25 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 
+
+
 using Sp16p3g8MobileApp;
 using Xamarin.Forms;
 using Newtonsoft.Json;
+using Xamarin.Forms.Platform.iOS;
+using Mono;
 
 [assembly: Dependency(typeof(ITicketStorage))]
 namespace Sp16p3g8MobileApp.iOS {
     class TicketStorageIOS : ITicketStorage {
         public List<string> LoadCodes(string filename) {
             //Loading according to Xamarin
+
             /* var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
             var filePath = Path.Combine(documentsPath , filename);
             string source = System.IO.File.ReadAllText(filePath);
-            List<String> = DeserializeJSONObjects(Source); */ //DEJSONIFY!
+
+            List<String> s = DeserializeJSONObjects<List<string>>(Source); */ //DEJSONIFY!
 
             throw new NotImplementedException();
         }
 
         public void Save(string filename , string NewCode) {
-            string JSONCode = JsonConvert.SerializeObject(NewCode); //JSONIFY!
+            List<string> codes = LoadCodes(filename);
+            codes.Add(NewCode);
+            string JSONCode = JsonConvert.SerializeObject(codes); //JSONIFY!
+
             /*The code according to Xamarin
             var documentsPath = Environment.GetFolderPath (Environment.SpecialFolder.Personal);
             var filePath = Path.Combine (documentsPath, filename);
