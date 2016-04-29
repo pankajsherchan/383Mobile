951d557b9487dda2b5c590ff97b154e2e4025f97
kevincarpenterdev <kevin.carpenter.dev@gmail.com>
2016-04-29 16:15:25 -0500

    Ticket Confirmation page (TODO: Finish Back button functionality)

---
+++ Sp16-p3-g8MobileApp/Sp16-p3-g8MobileApp/Views/Ticket/ConfirmationPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Sp16p3g8MobileApp {
+    public class ConfirmationPage : ContentPage {
+        public ConfirmationPage(string URL) {
+
+            //Call example
+            //new NavigationPage(new ConfirmationPage("http://www.robertsspaceindustries.com"));
+
+            Content = new StackLayout {
+                Children = {
+                    new Button() { Text="Go back" }, new WebView() { Source = URL, VerticalOptions=LayoutOptions.FillAndExpand }
+                }, Spacing=10
+            };
+        }
+    }
+}
